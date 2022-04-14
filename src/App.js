03aea2a09fdc8307401d666ce6f3a9db0e4b542e
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2022-04-13 22:44:24 -0400

    created Announcements.js and Checklist.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          To do
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dashboard</h1>
       </header>
+      <body>
+      <p>Announcements</p>
+      <p>Checklist</p>
+      </body>
     </div>
   );
 }
