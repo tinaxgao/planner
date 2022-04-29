0cd31bc544e00d497c0af764ae31fd9655d9a9ea
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2022-04-29 18:29:10 -0400

    import fonts

--- src/App.js
+++ src/App.js
@@ -9,11 +9,21 @@ function App() {
       <header className="App-header">
         <h1>Half-Peach Bakery</h1>
       </header>
-      
+      <nav id="user-nav">
+        <h4>Manager</h4>
+        <h4>Employee1</h4>
+        <h4>Employee2</h4>
+      </nav>
+
       <Announcements />
-      <section><h2>Checklist</h2><Checklist /></section>
-      <section><h2>Shopping List</h2><ShoppingList /></section>
-      
+      <section>
+        <h2>Checklist</h2>
+        <Checklist />
+      </section>
+      <section>
+        <h2>Shopping List</h2>
+        <ShoppingList />
+      </section>
     </div>
   );
 }
