ffaa418da91d5156c452c03d58026aeea579d995
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2022-05-04 16:14:27 -0400

    App.css top nav

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Checklist from "./components/checklist/Checklist";
 import ShoppingList from "./components/shoppinglist/ShoppingList";
 import BottomNav from "./components/navs/BottomNav";
 
+import AppIcon from "./icons/grape.svg";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Restaurant Name</h1>
+        <img src={AppIcon} alt="logo" />
+        <nav id="user-nav">
+          <h4>Manager</h4>
+          <h4>Employee1</h4>
+          <h4>Employee2</h4>
+          <h4>Employee3</h4>
+          <h4>Employee4</h4>
+        </nav>
       </header>
-      <nav id="user-nav">
-        <h4>Manager</h4>
-        <h4>Employee1</h4>
-        <h4>Employee2</h4>
-        <h4>Employee3</h4>
-      </nav>
 
       <Announcements />
       <section>
