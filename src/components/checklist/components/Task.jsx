31cdc5b7ce44c19a11cf9b75723c28d415fd43e7
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2022-08-19 17:35:37 -0400

    wip

--- src/components/checklist/components/Task.jsx
+++ src/components/checklist/components/Task.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import { useDrag } from "react-dnd";
 import { useDispatch } from "react-redux";
 import { removeTask } from "../checklistSlice";
+import { ITEM } from "../../constants";
 
 const Task = ({ task, index }) => {
   const dispatch = useDispatch();
-  
-  const [{ isDragging }, drag] = useDrag({
-    type: "item",
+
+  const [{ opacity }, drag] = useDrag({
+    type: ITEM,
     collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
+      opacity: monitor.isDragging() ? 0.2 : 1,
     }),
   });
 
   return (
-    <div className="checklist">
+    <div className="checklist" ref={drag} style={{ opacity }}>
       <label className="toplabel">Task</label>
       <h3>
         id:{task.id} / content:{task.content} / index:{index}
--- src/components/checklist/components/TaskList.jsx
+++ src/components/checklist/components/TaskList.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Task from "./Task";
+import { DropZone } from "../../../utils/DnD/DropZone";
 
 const TaskList = () => {
   const tasks = useSelector((state) => state.checklist);
 
   return (
     <div>
+      <DropZone data={{ path: "0-0" }} />
       {tasks.checklist.map((i, index) => (
-        <Task key={i.id} task={i} index={index} />
+        <React.Fragment key={i.id}>
+          <DropZone data={{ path: `${index}-0` }} />
+          <Task task={i} index={index} />
+          <DropZone data={{ path: `${index}-1` }} />
+        </React.Fragment>
       ))}
+      <DropZone data={{ path: "0-0" }} isLast />
     </div>
   );
 };
